b755fe2199ece349326b58533930bab54c20764d
Dmitry Saprykin <saprykin.dmitry@gmail.com>
2013-01-30 16:30:00 +0400

    * replication

---
+++ example/replication.php
@@ -0,0 +1,10 @@
+<?php
+ 
+set_include_path(get_include_path().PATH_SEPARATOR.dirname(__FILE__));
+include_once 'JsonRpc.php';
+$config = include 'config.php';
+
+$connect = new JsonRpc_Connection($config['host'], $config['port']);
+$feature = $connect->request('rep_status', array());
+$connect->process();
+var_dump($feature->get());
